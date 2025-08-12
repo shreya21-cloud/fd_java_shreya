bcd78a63a7bbcb5d658599137967042e6f89669b
shreya sharma <sharmashreyaa21@gmail.com>
2025-08-12 09:50:09 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,8 @@
+public class array {
+    public static void main (String[] args) {
+        int[]b = {3,6,7};
+        int c[] = {3,8,7};
+        System.out.println(b[1]);
+        System.out.println(c[2]);
+    }
+}
